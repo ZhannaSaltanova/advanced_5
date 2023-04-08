5526854d9a1cd20573c191933c995783111b66d5
Saltanova Zhanna <nedikovazhanna@gmail.com>
2023-04-08 13:49:00 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
+import { CensorComponent } from './pages/censor/censor.component';
+import { UserslistComponent } from './pages/userslist/userslist.component';
+import { TasklistComponent } from './pages/tasklist/tasklist.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'works', component: WorksComponent, children: [
+    { path: 'censor', component: CensorComponent },
+    { path: 'userslist', component: UserslistComponent },
+    { path: 'tasklist', component: TasklistComponent },
+    ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
